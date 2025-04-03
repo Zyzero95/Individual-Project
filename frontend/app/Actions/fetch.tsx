ab210954b14799fb901ce01764c02bc8400b2acc
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-04-03 10:41:25 +0200

    Implemented fetch function for fetching animal by id

--- frontend/app/Actions/fetch.tsx
+++ frontend/app/Actions/fetch.tsx
@@ -14,12 +14,26 @@ export default async function fetchAllAnimals(): Promise<AnimalInterface[] | nul
   }
   catch(error: unknown) {
     if(error instanceof Error){
-        throw(error);
+        console.error(error.message);
     }
   }
   return null;
 }
 
-export async function fetchAnimal() {
+export async function fetchAnimal(id: number): Promise<AnimalInterface | null> {
+    try {
+        const response = await fetch(`${url}/${id}`);
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
 
+        const json = await response.json();
+        return json;
+
+    } catch (error: unknown) {
+        if(error instanceof Error){
+            console.error(error.message);
+        }
+    }
+    return null;
 }
