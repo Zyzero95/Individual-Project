b2f455e836379a022aa13a058f5fdef9f8b291cb
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-04-04 15:12:59 +0200

    Added hamburger menu with useState and CSS classes. Added styling for Hamburger menu. (Needs better styling)

--- frontend/app/Components/Header/header.tsx
+++ frontend/app/Components/Header/header.tsx
@@ -1,11 +1,35 @@
+'use client'
 import Link from "next/link";
 import styles from "./header.module.css"
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
 
 
 export default function Header() {
+
+    const [menuToggle, setMenuToggle] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuToggle(toggle => !toggle);
+    }
+
   return (
     <header className={styles.headerSection}>
         <h1 className={styles.headerTitle}>Animal Facts</h1>
+        <section className={styles.headerMobileMenu}>
+            <Menu className={menuToggle === true ? styles.lucideMenuIconToggled : styles.lucideMenuIcon} onClick={toggleMenu} size={48}/>
+            <X className={menuToggle === true ? styles.lucideMenuIconCloseToggled : styles.lucideMenuIconClose} onClick={toggleMenu} size={48}/>
+            <section className={menuToggle === true ? styles.headerMobile : styles.headerMobileToggled}>
+                <nav className={styles.headerNavMobile}>
+                <ul>
+                    <li><Link href="/"/>Home</li>
+                    <li><Link href="/about"/>About</li>
+                    <li><Link href=""/>Random Animal</li>
+                </ul>
+                </nav>
+            </section>
+        </section>
+        
         <nav className={styles.headerNav}>
             <ul>
                 <li><Link href="/"/>Home</li>
