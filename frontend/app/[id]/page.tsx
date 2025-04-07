2ab04552e977e093854b863f76f1affb84090861
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-04-07 16:54:44 +0200

    Added JSX elements to ID page as a base for styling.

--- frontend/app/[id]/page.tsx
+++ frontend/app/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { fetchAnimal } from "../Actions/fetch";
 import styles from "./page.module.css"
+import anicardStyle from "../Components/Animal Card/animalcard.module.css"
 
 export default async function page({ params }: {params: Promise<{id: number}>}) {
 
@@ -10,6 +11,12 @@ export default async function page({ params }: {params: Promise<{id: number}>}) 
   return (
     <main className={styles.idPage}>
       <h2>{data?.name}</h2>
+      <section className={anicardStyle.anicardEnvSection}>
+        {data?.environment.map((env) => <p key={env} className={`${anicardStyle.anicardEnv}${env}`}>{env == "CoralReef" ? "Coral Reef" : env == "DeepOcean" ? "Deep Ocean" : env == "RockyReefs" ? "Rocky Reefs": env}</p>)}
+      </section>
+      <p>{data?.species}</p>
+      <p>{data?.foodPreference}</p>
+      <p>{data?.description}</p>
     </main>
   )
 }
