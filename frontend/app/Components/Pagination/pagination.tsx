3d58f9b86f9e39bc1abfea6f8adc527a9c366bb6
Viktor Lofgren <johan.viktor.lofgren@live.com>
2025-04-11 11:24:23 +0200

    Imported icons from Lucide for Pagination Component

--- frontend/app/Components/Pagination/pagination.tsx
+++ frontend/app/Components/Pagination/pagination.tsx
@@ -3,6 +3,7 @@
 import { useRouter, useSearchParams } from "next/navigation"
 import { FC } from "react";
 import styles from "./pagination.module.css"
+import { ArrowLeft, ArrowRight } from "lucide-react";
 
 interface PaginationProps {
     hasNextPage: boolean,
@@ -22,9 +23,9 @@ const Pagination: FC<PaginationProps> = ({
     return (
         <section className={styles.paginationWrapper}>
             <section className={styles.paginationContainer}>
-                <button className={styles.paginationButton} disabled={!hasPrevPage} onClick={() =>router.push(`/?page=${Number(page) - 1}&per_page=${perPage}`)}>Prev page</button>
+                <button className={styles.paginationButton} disabled={!hasPrevPage} onClick={() =>router.push(`/?page=${Number(page) - 1}&per_page=${perPage}`)}><ArrowLeft/></button>
                 <p className={styles.paginationParagraph}>{page} / {Math.ceil(60 / Number(perPage))}</p>
-                <button className={styles.paginationButton} disabled={!hasNextPage} onClick={() => router.push(`/?page=${Number(page) + 1}&per_page=${perPage}`)}>Next page</button>
+                <button className={styles.paginationButton} disabled={!hasNextPage} onClick={() => router.push(`/?page=${Number(page) + 1}&per_page=${perPage}`)}><ArrowRight/></button>
             </section>
         </section>
     )
